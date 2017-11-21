8b4fe82c0e66a58cd4fdefc04426dc602d0ff06a
Patrick <Patrick@10.1.54.167>
2017-11-21 12:41:01 -0500

    init commit

---
+++ ProductionLine/src/Disk.java
@@ -0,0 +1,23 @@
+/**
+ * this class represents a disk on the production line with a given radius
+ */
+
+/**
+ * @author Patrick
+ *
+ */
+
+public class Disk implements Comparable<Disk> {
+	
+	private int radius;
+
+	public int getRadius() {
+		return radius;
+	}
+	
+	@Override
+	public int compareTo(Disk d2) {
+		return radius - d2.getRadius();
+	}
+
+}
